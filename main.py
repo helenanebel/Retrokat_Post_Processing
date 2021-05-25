e29a67eac4a3a19101d0fee2e709f2dff902fb45
Helena Nebel <helena.nebel@uni-tuebingen.de>
2021-05-25 11:11:38 +0200

    Initial commit

--- main.py
+++ main.py
@@ -2,5 +2,4 @@ from transform_MARCXML import transform, check_and_split_in_issues
 
 if __name__ == '__main__':
     # check_and_split_in_issues()
-    transform('145', [r'Presenting\sthe', r'Index\sto', r'Book\sReview:\sBooks\sReceived', r'Cumulative\sIndex\sVolumes\sI-XV\s(1971–1985)', r'Other\sBooks\sReceived'],
-              (1971, 2017))
+    transform('394', [], (1992, 2016))
