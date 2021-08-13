629a8c46b52b71f9e4cf1034278714d5c11be89e
Helena Nebel <helena.nebel@uni-tuebingen.de>
2021-08-13 09:36:55 +0200

    Merge records from separate folder

--- merge_multiple_crawls.py
+++ merge_multiple_crawls.py
@@ -1,15 +1,16 @@
 import os
 import xml.etree.ElementTree as ElementTree
 
+
 def merge_journal_records():
     ElementTree.register_namespace('', "http://www.loc.gov/MARC21/slim")
     complete_tree = ElementTree.parse('marcxml_empty.xml')
     complete_root = complete_tree.getroot()
     zeder_id = input('Bitte geben sie die ZEDER-ID der zusammenzuführenden Records ein: ')
     record_nr = 0
-    for file in os.listdir('result_files'):
-        if zeder_id + '_' in file:
-            tree = ElementTree.parse('result_files/' + file)
+    for file in os.listdir('multiple_crawl'):
+        if zeder_id in file:
+            tree = ElementTree.parse('multiple_crawl/' + file)
             root = tree.getroot()
             records = root.findall('.//{http://www.loc.gov/MARC21/slim}record')
             records = [record for record in records]
@@ -19,6 +20,7 @@ def merge_journal_records():
                     print(record_nr)
                 complete_root.append(record)
     complete_tree.write('result_files/' + zeder_id + '.xml', encoding='utf-8', xml_declaration=True)
+
 
 if __name__ == '__main__':
     merge_journal_records()
