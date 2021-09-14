f2b41576caa32563b22a41f5059b960acc75b2b8
Helena Nebel <helena.nebel@uni-tuebingen.de>
2021-09-14 13:12:48 +0200

    Append new lines

--- Rename files for FTP & move.py
+++ Rename files for FTP & move.py
@@ -15,7 +15,7 @@ for file in os.listdir('proper_files'):
         copy2('proper_files/' + file, 'W:/FID-Projekte/Team Retro-Scan/Zotero/Einspielen_TestDB/' + new_filename_for_ftp)
         files_renamed += file.ljust(16) + '=     ' + new_filename_for_ftp + '\n'
         move_to_scp_server += '\npython3 upload_to_bsz_ftp_server.py ' + new_filename_for_ftp + ' /pub/UBTuebingen_Default_Test/'
-with open('W:/FID-Projekte/Team Retro-Scan/Zotero/Einspielen_TestDB/files_renamed_on_' + timestamp + '.txt', 'w') as doku_file:
+with open('W:/FID-Projekte/Team Retro-Scan/Zotero/Einspielen_TestDB/files_renamed_on_' + timestamp + '.txt', 'a') as doku_file:
     doku_file.write(files_renamed)
 
 print(str(file_nr - 1), 'files renamed and moved to folder')
