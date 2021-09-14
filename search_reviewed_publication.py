e1ce70ea560a2d3054857d844297298f894dbd30
Helena Nebel <helena.nebel@uni-tuebingen.de>
2021-09-14 13:13:55 +0200

    Prioritize found ppns

--- search_reviewed_publication.py
+++ search_reviewed_publication.py
@@ -4,13 +4,53 @@ import re
 import unidecode
 
 
+def check_response_for_priority_of_results(xml_soup):
+    # print(xml_soup.find('zs:numberofrecords'))
+    SWB_ppns = []
+    theo_ppns = []
+    other_ppns = []
+    if xml_soup.find('zs:numberofrecords').text != '0':
+        for record in xml_soup.find_all('record'):
+            ppn = record.find('datafield', tag='003@').find('subfield', code='0').text
+            ixtheo = False
+            # hier den Record auswählen, der eine 1 als SSG Zeichen hat ODER ein anderes Kennzeichen
+            ssg_tags = [element.find('subfield', code='a').text for element in record.find_all('datafield', tag='045V')]
+            for ssg_tag in ['1', '0', '6,22']:
+                if ssg_tag in ssg_tags:
+                    ixtheo = True
+                    print('found ssg')
+                    break
+            if not ixtheo:
+                cods = [element.find('subfield', code='a').text for element in record.find_all('datafield', tag='016B')]
+                for cod in ['mteo', 'redo', 'DTH5', 'AUGU', 'DAKR', 'MIKA', 'BIIN', 'KALD', 'GIRA']:
+                    if cod in cods:
+                        ixtheo = True
+                        print('found cod')
+                        break
+            if ixtheo:
+                theo_ppns.append(ppn)
+            elif record.find('datafield', tag='007G').find('subfield', code='i').text == 'BSZ':
+                SWB_ppns.append(ppn)
+            else:
+                other_ppns.append(ppn)
+    # print(theo_ppns)
+    # print(SWB_ppns)
+    # print(other_ppns)
+    if theo_ppns:
+        return theo_ppns, True
+    elif SWB_ppns:
+        return SWB_ppns, False
+    else:
+        return other_ppns, False
+
+
 def encode_in_ascii_and_remove_whitespaces_and_points(string: str, is_author, is_place):
     if is_author:
         list_of_letters = re.findall(r'[\w,]+', string)
     elif is_place:
         list_of_letters = [re.findall(r'[\w]+', string)[0]]
     else:
-        list_of_letters = re.findall(r'\w+', string)
+        list_of_letters = [word if word.lower() not in ['all', 'or', 'and'] else "'" + word for word in re.findall(r'\w+', string)]
     decoded_string = '+'.join(list_of_letters)
     decoded_string = unidecode.unidecode(decoded_string)
     decoded_string = decoded_string.strip('+')
@@ -31,26 +71,30 @@ def search_publication(title, author, year, place):
     url = 'http://sru.k10plus.de/opac-de-627?version=1.1&operation=searchRetrieve&query='
     for key in pub_dict:
         if pub_dict[key]:
-            url += 'pica.' + key + '%3D' + pub_dict[key].strip('.') + '+and+'
+            if pub_dict[key] != 'null':
+                url += 'pica.' + key + '%3D' + pub_dict[key].strip('.') + '+and+'
     url = url.strip('+and+') + '&maximumRecords=10&recordSchema=picaxml'
+    print(url)
     xml_data = urllib.request.urlopen(url)
     xml_soup = BeautifulSoup(xml_data, features='lxml')
-    # print(xml_soup.find('zs:numberofrecords'))
-    if xml_soup.find('zs:numberofrecords').text != '0':
-        # hier den Record auswählen, der eine 1 als SSG Zeichen hat ODER ein anderes Kennzeichen
-        if '1' not in [element.find('subfield', code='a').text for element in xml_soup.find_all('datafield', tag='045V')]:
-            print(url)
-            print(xml_soup.find('datafield', tag='007G').find('subfield', code='i').text)
-            print(xml_soup.find('datafield', tag='003@').find('subfield', code='0').text)
+    ppns, is_theo = check_response_for_priority_of_results(xml_soup)
+    return ppns, is_theo
 
 
+def search_publication_with_isbn(isbn: str):
+    url = 'http://sru.k10plus.de/opac-de-627?version=1.1&operation=searchRetrieve&query=pica.isb%3D' + isbn + '&maximumRecords=10&recordSchema=picaxml'
+    print(url)
+    xml_data = urllib.request.urlopen(url)
+    xml_soup = BeautifulSoup(xml_data, features='lxml')
+    ppns, is_theo = check_response_for_priority_of_results(xml_soup)
+    return ppns, is_theo
 
 
 if __name__ == '__main__':
-    search_publication('The Semantic Field of Cutting Tools in Biblical Hebrew', 'koller, aaron j.', '2012', 'washington')
+    # search_publication('The Semantic Field of Cutting Tools in Biblical Hebrew', 'koller, aaron j.', '2012', 'washington')
+    search_publication_with_isbn('Book Reviews: Three Thomist Studies. By Frederick E. Crowe, SJ (Supplementary issue of Lonergan Workshop, vol. 16, edited by Michael Vertin and Frederick Lawrence.) Boston: Boston College, 2000. Pp. xxiv+260. ISBN 0-9700862-0-2')
 
-# Verlagsname und -ort verwenden, prüfen, ob SSG-Kennzeichen vorhanden & in Liste schreiben, falls nicht (für Überprüfung an Herrn Faßnacht).
-# mit Dubletten umgehen (immer vorzugsweise die SWB-Aufnahme, als erstes unsere, verwenden)
-# zuerst gekennzeichnete, dann wenn möglich SWB-Aufnahmen.
+# prüfen, ob SSG-Kennzeichen vorhanden & in Liste schreiben, falls nicht (für Überprüfung an Herrn Faßnacht).
+# mit Dubletten umgehen (immer vorzugsweise die gekennzeichnete, dann andere SWB-Aufnahmen verwenden)
 # Abrufzeichen in W:\FID-Projekte\Zweitveröffentlichung\Arbeitsunterlagen_Hilfsmittel_Links
 # in der Datei Abrufzeichen IxTheo.pdf prüfen, alles, was hier zutrifft, übernehmen.
