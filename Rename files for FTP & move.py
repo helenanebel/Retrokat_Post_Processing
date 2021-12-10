4115b03e80dcfb3632c761e3252b27817f5f2533
Helena Nebel <helena.nebel@uni-tuebingen.de>
2021-12-10 17:10:21 +0100

    Implement continuous count for files with same date

--- Rename files for FTP & move.py
+++ Rename files for FTP & move.py
@@ -6,13 +6,16 @@ import json
 move_to_scp_server = ''
 commands = ''
 timestamp = strftime('%y%m%d')
-file_nr = 1
+file_nr = 0
+file_nr_for_filename = 1
 with open('W:/FID-Projekte/Team Retro-Scan/Zotero/Einspielen_TestDB/files_renamed.json', 'r') as renamed_files_file:
     renamed_files = json.load(renamed_files_file)
+    file_nr_for_filename += len([renamed_files[filename] for filename in renamed_files if timestamp + '_' in renamed_files[filename]])
     for file in os.listdir('proper_files'):
         if file not in renamed_files:
+            file_nr_for_filename += 1
             file_nr += 1
-            new_filename_for_ftp = 'ixtheo_zotero_' + timestamp + '_' + str(file_nr).zfill(3) + '.xml'
+            new_filename_for_ftp = 'ixtheo_zotero_' + timestamp + '_' + str(file_nr_for_filename).zfill(3) + '.xml'
             copy2('proper_files/' + file, 'W:/FID-Projekte/Team Retro-Scan/Zotero/Einspielen_TestDB/' + new_filename_for_ftp)
             renamed_files[file] = new_filename_for_ftp
             move_to_scp_server += '\npython3 upload_to_bsz_ftp_server.py ' + new_filename_for_ftp + ' /pub/UBTuebingen_Default_Test/'
@@ -20,7 +23,7 @@ with open('W:/FID-Projekte/Team Retro-Scan/Zotero/Einspielen_TestDB/files_rename
 with open('W:/FID-Projekte/Team Retro-Scan/Zotero/Einspielen_TestDB/files_renamed.json', 'w') as renamed_files_file:
     json.dump(renamed_files, renamed_files_file)
 
-print(str(file_nr - 1), 'files renamed and moved to folder')
+print(str(file_nr), 'files renamed and moved to folder')
 
 print('Commands:')
 print('Dateien auf BENU verschieben:\nW:\ncd /FID-Projekte/Team Retro-Scan/Zotero/Einspielen_TestDB'
