d9fe96eb9a442f9a7399ba83fee545b6910528c0
Helena Nebel <helena.nebel@uni-tuebingen.de>
2021-12-10 17:09:02 +0100

    Catch caused by from malformed xml-files

--- merge_multiple_crawls.py
+++ merge_multiple_crawls.py
@@ -6,19 +6,22 @@ def merge_journal_records():
     ElementTree.register_namespace('', "http://www.loc.gov/MARC21/slim")
     complete_tree = ElementTree.parse('marcxml_empty.xml')
     complete_root = complete_tree.getroot()
-    zeder_id = input('Bitte geben sie die ZEDER-ID der zusammenzuführenden Records ein: ')
+    zeder_id = input('Bitte geben Sie die ZEDER-ID der zusammenzuführenden Records ein: ')
     record_nr = 0
     for file in os.listdir('multiple_crawl'):
         if zeder_id in file:
-            tree = ElementTree.parse('multiple_crawl/' + file)
-            root = tree.getroot()
-            records = root.findall('.//{http://www.loc.gov/MARC21/slim}record')
-            records = [record for record in records]
-            for record in records:
-                record_nr += 1
-                if record_nr%200 == 0:
-                    print(record_nr)
-                complete_root.append(record)
+            try:
+                tree = ElementTree.parse('multiple_crawl/' + file)
+                root = tree.getroot()
+                records = root.findall('.//{http://www.loc.gov/MARC21/slim}record')
+                records = [record for record in records]
+                for record in records:
+                    record_nr += 1
+                    if record_nr%200 == 0:
+                        print(record_nr)
+                    complete_root.append(record)
+            except:
+                print(file)
     complete_tree.write('result_files/' + zeder_id + '.xml', encoding='utf-8', xml_declaration=True)
 
 
