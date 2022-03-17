974fb116d7532c7e5a24c66de2f14685a3b6aa90
Helena Nebel <helena.nebel@uni-tuebingen.de>
2022-03-17 16:52:14 +0100

    Limit total number of records moved to ftp

--- Move files to productive system.py
+++ Move files to productive system.py
@@ -2,12 +2,15 @@ import os
 from shutil import copy2
 from time import strftime
 import json
+import xml.etree.ElementTree as ElementTree
+from termcolor import colored
 
 move_to_scp_server = ''
 commands = ''
 timestamp = strftime('%y%m%d')
+total_record_number = 0
 file_nr = 0
-file_nr_for_filename = 1
+file_nr_for_filename = 4
 with open('W:/FID-Projekte/Team Retro-Scan/Zotero/Einspielen_ProduktivDB/files_renamed.json', 'r') as renamed_files_file:
     renamed_files = json.load(renamed_files_file)
     file_nr_for_filename += len(
@@ -17,6 +20,18 @@ with open('W:/FID-Projekte/Team Retro-Scan/Zotero/Einspielen_ProduktivDB/files_r
             print(file)
             continue
         if file not in renamed_files:
+            ElementTree.register_namespace('', "http://www.loc.gov/MARC21/slim")
+            result_tree = ElementTree.parse('final_files/' + file)
+            result_root = result_tree.getroot()
+            records = result_root.findall('.//{http://www.loc.gov/MARC21/slim}record')
+            total_record_number += len(records)
+            if total_record_number > 18000:
+                print(colored('total record number exceeds maximum capazity of ftp-server.', 'red'))
+                continue
+            paginations = ['-']
+            reviews = 0
+            current_issue = 'first'
+            records = [record for record in records]
             file_nr_for_filename += 1
             file_nr += 1
             new_filename_for_ftp = 'ixtheo_zotero_' + timestamp + '_' + str(file_nr_for_filename).zfill(3) + '.xml'
@@ -28,9 +43,10 @@ with open('W:/FID-Projekte/Team Retro-Scan/Zotero/Einspielen_ProduktivDB/files_r
     json.dump(renamed_files, renamed_files_file)
 
 print(str(file_nr), 'files renamed and moved to folder')
+print(total_record_number, 'records moved to folder')
 
 print('Commands:')
-print('Dateien auf BENU verschieben:\nW:\ncd /FID-Projekte/Team Retro-Scan/Zotero/Einspielen_ProduktivDB'
-      '\nscp ixtheo_zotero_' + timestamp + '_*.xml hnebel@benu.ub.uni-tuebingen.de:/home/hnebel/bsz')
-print('Dateien von BENU auf den FTP-Server legen:\nssh hnebel@benu.ub.uni-tuebingen.de\ncd /home/hnebel/bsz'
-      + move_to_scp_server)
+print('Dateien auf BENU verschieben & von BENU auf den FTP-Server legen:'
+      '\nW:\ncd /FID-Projekte/Team Retro-Scan/Zotero/Einspielen_ProduktivDB'
+      '\nscp ixtheo_zotero_' + timestamp + '_*.xml hnebel@benu.ub.uni-tuebingen.de:/home/hnebel/bsz'
+      '\n\nssh hnebel@benu.ub.uni-tuebingen.de\ncd /home/hnebel/bsz' + move_to_scp_server)
