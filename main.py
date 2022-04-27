1bef2641c1a4c9e9c55caf70e3bb8832b79d5886
Helena Nebel <helena.nebel@uni-tuebingen.de>
2022-04-27 14:05:06 +0200

    Add review-information from JSTOR-Data

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import add_missing_links
 from transform_MARCXML import transform, check_and_split_in_issues
 import json
 from create_jstor_link_dict import get_jstor_links
+from get_review_information_from_jstor_data import get_review_information
 
 if __name__ == '__main__':
     zeder_id = input('Bitte geben Sie die ZEDER-ID ein: ')
@@ -15,23 +16,20 @@ if __name__ == '__main__':
                 conf_available = True
                 if 'eppn' not in conf_dict[zeder_id]:
                     conf_dict[zeder_id]['eppn'] = input('Bitte geben Sie die ePPN ein: ')
-                    with open('W:/FID-Projekte/Team Retro-Scan/Zotero/conf.json', 'w') as conf_file:
-                        json.dump(conf_dict, conf_file)
                 if 'lang' not in conf_dict[zeder_id]:
                     conf_dict[zeder_id]['lang'] = input('Bitte geben Sie die Default-Sprache ein: ')
-                    with open('W:/FID-Projekte/Team Retro-Scan/Zotero/conf.json', 'w') as conf_file:
-                        json.dump(conf_dict, conf_file)
                 if 'conf_langs' not in conf_dict[zeder_id]:
                     langs = input('Bitte geben Sie die zulässigen Sprachen ein: ')
                     lang_dict = json.loads(langs)
                     conf_dict[zeder_id]['conf_langs'] = lang_dict
-                    with open('W:/FID-Projekte/Team Retro-Scan/Zotero/conf.json', 'w') as conf_file:
-                        json.dump(conf_dict, conf_file)
                 if 'detect_review_langs' not in conf_dict[zeder_id]:
                     detect_review_langs = bool(input('Bitte geben Sie an, ob bei Rezensionen Spracherkennung durchgeführt werden soll: '))
                     conf_dict[zeder_id]['detect_review_langs'] = detect_review_langs
-                    with open('W:/FID-Projekte/Team Retro-Scan/Zotero/conf.json', 'w') as conf_file:
-                        json.dump(conf_dict, conf_file)
+                if 'add_jstor_data_from_file' not in conf_dict[zeder_id]:
+                    add_jstor_data = input('Bitte geben Sie an, aus welcher Datei Daten übernommen werden sollen: ')
+                    conf_dict[zeder_id]['add_jstor_data_from_file'] = add_jstor_data
+                with open('W:/FID-Projekte/Team Retro-Scan/Zotero/conf.json', 'w') as conf_file:
+                    json.dump(conf_dict, conf_file)
     # if zeder_id + '.json' not in os.listdir('W:/FID-Projekte/Team Retro-Scan/Zotero/jstor_mapping'):
     get_jstor_links(zeder_id)
     record_nr = check_and_split_in_issues(zeder_id, conf_available)
@@ -52,7 +50,8 @@ if __name__ == '__main__':
         langs = input('Bitte geben Sie die zulässigen Sprachen ein: ')
         conf_langs = json.loads(langs)
         detect_review_langs = bool(input('Bitte geben Sie an, ob bei Rezensionen Spracherkennung durchgeführt werden soll: '))
-        conf_dict[zeder_id] = {'exclude': exclude, 'period': period, 'eppn': eppn, 'lang': default_lang, 'conf_langs': conf_langs, 'detect_review_langs': detect_review_langs}
+        add_jstor_data = input('Bitte geben Sie an, aus welcher Datei Daten übernommen werden sollen: ')
+        conf_dict[zeder_id] = {'exclude': exclude, 'period': period, 'eppn': eppn, 'lang': default_lang, 'conf_langs': conf_langs, 'detect_review_langs': detect_review_langs, 'add_jstor_data_from_file': add_jstor_data}
         if input('Wollen Sie diese Konfigurationsangaben speichern (j/n)') == 'j':
             with open('W:/FID-Projekte/Team Retro-Scan/Zotero/conf.json', 'w') as conf_file:
                 json.dump(conf_dict, conf_file)
@@ -65,4 +64,6 @@ if __name__ == '__main__':
     default_lang = conf_dict[zeder_id]['lang']
     conf_langs = conf_dict[zeder_id]['conf_langs']
     detect_review_langs = conf_dict[zeder_id]['detect_review_langs']
-    transform(zeder_id, exclude, period, record_nr, default_lang, conf_langs, detect_review_langs)
+    add_jstor_data = conf_dict[zeder_id]['add_jstor_data_from_file']
+    get_review_information(zeder_id, add_jstor_data)
+    transform(zeder_id, exclude, period, record_nr, default_lang, conf_langs, detect_review_langs, add_jstor_data)
