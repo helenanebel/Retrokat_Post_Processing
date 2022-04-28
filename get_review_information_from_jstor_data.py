46c1e50ef1b1abe5c8fc13e6084b7d37442477cf
Helena Nebel <helena.nebel@uni-tuebingen.de>
2022-04-28 09:56:53 +0200

    Add review-information from JSTOR-Data

---
+++ get_review_information_from_jstor_data.py
@@ -0,0 +1,23 @@
+import os
+import re
+import json
+import xml.etree.ElementTree as ElementTree
+
+
+def get_review_information(zeder_id, jstor_zeder_id):
+    if zeder_id + '_reviews.json' not in os.listdir('W:/FID-Projekte/Team Retro-Scan/Zotero/jstor_json'):
+        review_dict = {}
+        jstor_tree = ElementTree.parse('result_files/' + jstor_zeder_id + '.xml')
+        jstor_root = jstor_tree.getroot()
+        ElementTree.register_namespace('', "http://www.loc.gov/MARC21/slim")
+        records = jstor_root.findall('.//{http://www.loc.gov/MARC21/slim}record')
+        for record in records:
+            urls = record.findall('{http://www.loc.gov/MARC21/slim}datafield[@tag="856"]/{http://www.loc.gov/MARC21/slim}subfield[@code="u"]')
+            for url in urls:
+                tags = record.findall \
+                    ('{http://www.loc.gov/MARC21/slim}datafield[@tag="650"]/{http://www.loc.gov/MARC21/slim}subfield[@code="a"]')
+                for tag in tags:
+                    if re.search(r'^#reviewed_pub#', tag.text, re.IGNORECASE):
+                        review_dict[url.text] = tag.text
+        with open('W:/FID-Projekte/Team Retro-Scan/Zotero/jstor_mapping/' + zeder_id + '_reviews.json', 'w') as json_file:
+            json.dump(review_dict, json_file)
