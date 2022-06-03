f3e3d1bcc029bd3b66b4fd12d0c6c5ed0a66ea5e
Helena Nebel <helena.nebel@uni-tuebingen.de>
2022-06-03 16:21:07 +0200

    Create review links from jstor-dict

--- get_review_information_from_jstor_data.py
+++ get_review_information_from_jstor_data.py
@@ -5,7 +5,7 @@ import xml.etree.ElementTree as ElementTree
 
 
 def get_review_information(zeder_id, jstor_zeder_id):
-    if zeder_id + '_reviews.json' not in os.listdir('W:/FID-Projekte/Team Retro-Scan/Zotero/jstor_json'):
+    if zeder_id + '_reviews.json' not in os.listdir('W:/FID-Projekte/Team Retro-Scan/Zotero/jstor_mapping'):
         review_dict = {}
         jstor_tree = ElementTree.parse('result_files/' + jstor_zeder_id + '.xml')
         jstor_root = jstor_tree.getroot()
@@ -18,6 +18,8 @@ def get_review_information(zeder_id, jstor_zeder_id):
                     ('{http://www.loc.gov/MARC21/slim}datafield[@tag="650"]/{http://www.loc.gov/MARC21/slim}subfield[@code="a"]')
                 for tag in tags:
                     if re.search(r'^#reviewed_pub#', tag.text, re.IGNORECASE):
-                        review_dict[url.text] = tag.text
+                        if url.text not in review_dict:
+                            review_dict[url.text] = []
+                        review_dict[url.text].append(tag.text)
         with open('W:/FID-Projekte/Team Retro-Scan/Zotero/jstor_mapping/' + zeder_id + '_reviews.json', 'w') as json_file:
             json.dump(review_dict, json_file)
