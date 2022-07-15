64f7534a6b477ef976de7e11f2d8bee4efe05416
Helena Nebel <helena.nebel@uni-tuebingen.de>
2022-07-15 17:44:09 +0200

    Merge files from multiple crawls from Home-Directory

--- merge_multiple_crawls.py
+++ merge_multiple_crawls.py
@@ -8,25 +8,25 @@ def merge_journal_records():
     complete_tree = ElementTree.parse('marcxml_empty.xml')
     complete_root = complete_tree.getroot()
     zeder_id = input('Bitte geben Sie die ZEDER-ID der zusammenzuführenden Records ein: ')
-    zeder_id = zeder_id.replace('+', '\\+')
+    zeder_id_for_regex = zeder_id.replace('+', '\\+')
     record_nr = 0
-    for file in os.listdir('multiple_crawl'):
-        if re.findall('^' + zeder_id + '_', file) or file == zeder_id + '.xml':
-            try:
-                print(file)
-                tree = ElementTree.parse('multiple_crawl/' + file)
-                root = tree.getroot()
-                records = root.findall('.//{http://www.loc.gov/MARC21/slim}record')
-                records = [record for record in records]
-                for record in records:
-                    record_nr += 1
-                    if record_nr%200 == 0:
-                        print(record_nr)
-                    complete_root.append(record)
-            except:
-                print('no xml:', file)
+    if zeder_id in os.listdir('C:/Users/hnebel'):
+        for file in os.listdir('C:/Users/hnebel/' + zeder_id):
+            if re.findall('^' + zeder_id_for_regex + '_', file) or file == zeder_id + '.xml':
+                try:
+                    tree = ElementTree.parse('C:/Users/hnebel/' + zeder_id + '/' + file)
+                    root = tree.getroot()
+                    records = root.findall('.//{http://www.loc.gov/MARC21/slim}record')
+                    records = [record for record in records]
+                    for record in records:
+                        record_nr += 1
+                        if record_nr%200 == 0:
+                            print(record_nr)
+                        complete_root.append(record)
+                except:
+                    print('no xml:', file)
     print("total records:", record_nr)
-    complete_tree.write('result_files/' + zeder_id.replace('\\+', '+') + '.xml', encoding='utf-8', xml_declaration=True)
+    complete_tree.write('result_files/' + zeder_id + '.xml', encoding='utf-8', xml_declaration=True)
 
 
 if __name__ == '__main__':
