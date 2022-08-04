1db445c3287884e427abc1665185c1dcba4e352c
Helena Nebel <helena.nebel@uni-tuebingen.de>
2022-08-04 18:10:09 +0200

    Get URLs with unsuccessful crawl attempts

--- get_urls_download_failed.py
+++ get_urls_download_failed.py
@@ -21,19 +21,21 @@ def get_urls(zeder_id):
             if found_out_file:
                 with open(zeder_id + '.out', 'w') as resulting_out_file:
                     resulting_out_file.write(resulting_string)
-    if zeder_id + '.out' in os.listdir():
-        with open(zeder_id + '.out', 'r') as out_file:
-            text = out_file.read()
-            successfull_urls = re.findall(r'Generated 1 record\(s\) for item (\d+) ', text)
-            for successfull_url in successfull_urls:
-                all_items.append(successfull_url)
-            failing_urls = re.findall(r'\| (.+?) \{.+} download failed!', text, re.IGNORECASE)
-            failing_item_urls = re.findall(r'Converting item \d+ .+? \| (.+?) .+?\n.+? --> Skipping record with URL .+? because it is an undesired item type', text, re.IGNORECASE)
-            failing_urls += failing_item_urls
-            aborted_urls = re.findall(r'DIRECT @ ([^\s]+)\n.+\n.+\n', text, re.IGNORECASE)
-            failing_urls += aborted_urls
-        with open(zeder_id + '_failing_links.json', 'w') as file:
-            json.dump(failing_urls, file)
+        if zeder_id + '.out' in os.listdir():
+            with open(zeder_id + '.out', 'r') as out_file:
+                text = out_file.read()
+                successfull_urls = re.findall(r'Generated 1 record\(s\) for item (\d+) ', text)
+                for successfull_url in successfull_urls:
+                    all_items.append(successfull_url)
+                failing_urls = re.findall(r'\| (.+?) \{.+} download failed!', text, re.IGNORECASE)
+                failing_item_urls = re.findall(r'Converting item \d+ .+? \| (.+?) .+?\n.+? --> Skipping record with URL .+? because it is an undesired item type', text, re.IGNORECASE)
+                failing_urls += failing_item_urls
+                aborted_urls = re.findall(r'DIRECT @ ([^\s]+)\n.+\n.+\n', text, re.IGNORECASE)
+                failing_urls += aborted_urls
+                unsuccessfull_crawl_attempts = re.findall(r'Unsuccessful crawl attempt for (.+?)\n', text, re.IGNORECASE)
+                failing_urls += unsuccessfull_crawl_attempts
+            with open(zeder_id + '_failing_links.json', 'w') as file:
+                json.dump(failing_urls, file)
 
 
 if __name__ == '__main__':
