ffc6583bb68bc6952e952abd48e35329214de2bc
Helena Nebel <helena.nebel@uni-tuebingen.de>
2022-08-15 11:59:55 +0200

    Manage files on drive W:

--- Rename files for FTP & move.py
+++ Rename files for FTP & move.py
@@ -15,10 +15,10 @@ file_nr_for_filename = 1
 with open('W:/FID-Projekte/Team Retro-Scan/Zotero/Einspielen_TestDB/files_renamed.json', 'r') as renamed_files_file:
     renamed_files = json.load(renamed_files_file)
     file_nr_for_filename += len([renamed_files[filename] for filename in renamed_files if timestamp + '_' in renamed_files[filename]])
-    for file in os.listdir('proper_files'):
+    for file in os.listdir('W:/FID-Projekte/Team Retro-Scan/Zotero/proper_files/'):
         if file not in renamed_files:
             ElementTree.register_namespace('', "http://www.loc.gov/MARC21/slim")
-            result_tree = ElementTree.parse('proper_files/' + file)
+            result_tree = ElementTree.parse('W:/FID-Projekte/Team Retro-Scan/Zotero/proper_files/' + file)
             result_root = result_tree.getroot()
             records = result_root.findall('.//{http://www.loc.gov/MARC21/slim}record')
             total_record_number += len(records)
@@ -28,7 +28,7 @@ with open('W:/FID-Projekte/Team Retro-Scan/Zotero/Einspielen_TestDB/files_rename
             file_nr_for_filename += 1
             file_nr += 1
             new_filename_for_ftp = 'ixtheo_zotero_' + timestamp + '_' + str(file_nr_for_filename).zfill(3) + '.xml'
-            copy2('proper_files/' + file, 'W:/FID-Projekte/Team Retro-Scan/Zotero/Einspielen_TestDB/' + new_filename_for_ftp)
+            copy2('W:/FID-Projekte/Team Retro-Scan/Zotero/proper_files/' + file, 'W:/FID-Projekte/Team Retro-Scan/Zotero/Einspielen_TestDB/' + new_filename_for_ftp)
             renamed_files[file] = new_filename_for_ftp
             move_to_scp_server += '\npython3 upload_to_bsz_ftp_server.py ' + new_filename_for_ftp + ' /pub/UBTuebingen_Default_Test/'
 
