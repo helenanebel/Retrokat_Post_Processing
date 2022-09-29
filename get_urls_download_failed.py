1bc0839eb93594adba004b15ab1b748ab40a1c59
Helena Nebel <helena.nebel@uni-tuebingen.de>
2022-09-29 11:41:09 +0200

    Change path to directories

--- get_urls_download_failed.py
+++ get_urls_download_failed.py
@@ -7,22 +7,22 @@ def get_urls(zeder_id):
     all_items = []
     failing_urls = []
     if zeder_id + '_failing_links.json' not in os.listdir():
-        if zeder_id + '.out' not in os.listdir():
+        if zeder_id + '.out' not in os.listdir('W:/FID-Projekte/Team Retro-Scan/Zotero/results'):
             resulting_string = ""
             found_out_file = False
-            zeder_id = zeder_id.replace('+', '\+')
-            if zeder_id + '_out' in os.listdir('C:/Users/hnebel'):
-                for file in os.listdir('C:/Users/hnebel/' + zeder_id + '_out/'):
-                    if re.findall(r'^' + zeder_id + '_', file):
+            if zeder_id + '_out' in os.listdir('W:/FID-Projekte/Team Retro-Scan/Zotero/results'):
+                zid = zeder_id.replace('+', '\+')
+                for file in os.listdir('W:/FID-Projekte/Team Retro-Scan/Zotero/results/' + zeder_id + '_out/'):
+                    if re.findall(r'^' + zid + '_', file):
                         found_out_file = True
-                        with open('C:/Users/hnebel/' + zeder_id + '_out/' + file, 'r') as out_file:
+                        with open('W:/FID-Projekte/Team Retro-Scan/Zotero/results/' + zeder_id + '_out/' + file, 'r') as out_file:
                             text = out_file.read()
                             resulting_string += text + "\n"
             if found_out_file:
-                with open(zeder_id + '.out', 'w') as resulting_out_file:
+                with open('W:/FID-Projekte/Team Retro-Scan/Zotero/results/' + zeder_id + '.out', 'w') as resulting_out_file:
                     resulting_out_file.write(resulting_string)
-        if zeder_id + '.out' in os.listdir():
-            with open(zeder_id + '.out', 'r') as out_file:
+        if zeder_id + '.out' in os.listdir('W:/FID-Projekte/Team Retro-Scan/Zotero/results'):
+            with open('W:/FID-Projekte/Team Retro-Scan/Zotero/results/' + zeder_id + '.out', 'r') as out_file:
                 text = out_file.read()
                 successfull_urls = re.findall(r'Generated 1 record\(s\) for item (\d+) ', text)
                 for successfull_url in successfull_urls:
@@ -34,7 +34,10 @@ def get_urls(zeder_id):
                 failing_urls += aborted_urls
                 unsuccessfull_crawl_attempts = re.findall(r'Unsuccessful crawl attempt for (.+?)\n', text, re.IGNORECASE)
                 failing_urls += unsuccessfull_crawl_attempts
+                unsuccessfull_downloads = re.findall(r'\| (.+?) \{.+?\} download failed!', text, re.IGNORECASE)
+                failing_urls += unsuccessfull_downloads
             with open(zeder_id + '_failing_links.json', 'w') as file:
+                failing_urls = list(set(failing_urls))
                 json.dump(failing_urls, file)
 
 
