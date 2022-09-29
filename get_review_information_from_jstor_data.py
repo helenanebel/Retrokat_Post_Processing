ea2c9b3ffceb8fdc6d8f5fdfe74ae671f86c6bfd
Helena Nebel <helena.nebel@uni-tuebingen.de>
2022-09-29 11:40:43 +0200

    Change path

--- get_review_information_from_jstor_data.py
+++ get_review_information_from_jstor_data.py
@@ -7,7 +7,7 @@ import xml.etree.ElementTree as ElementTree
 def get_review_information(zeder_id, jstor_zeder_id):
     if zeder_id + '_reviews.json' not in os.listdir('W:/FID-Projekte/Team Retro-Scan/Zotero/jstor_mapping'):
         review_dict = {}
-        jstor_tree = ElementTree.parse('result_files/' + jstor_zeder_id + '.xml')
+        jstor_tree = ElementTree.parse('W:/FID-Projekte/Team Retro-Scan/Zotero/result_files/' + jstor_zeder_id + '.xml')
         jstor_root = jstor_tree.getroot()
         ElementTree.register_namespace('', "http://www.loc.gov/MARC21/slim")
         records = jstor_root.findall('.//{http://www.loc.gov/MARC21/slim}record')
