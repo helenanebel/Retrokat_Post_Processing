d86b0ac648e16685cc7c369be8f48b99efb12ff6
Helena Nebel <helena.nebel@uni-tuebingen.de>
2022-10-27 21:12:08 +0200

    Move credentials to server

--- get_k10plus_credentials.py
+++ get_k10plus_credentials.py
@@ -2,7 +2,7 @@ import json
 
 
 def get_credentials():
-    with open("k10plus_credentials.json", "r") as credential_file:
+    with open("W:/FID-Projekte/Team Retro-Scan/Zotero/BENU/k10plus_credentials.json", "r") as credential_file:
         credential_dict = json.load(credential_file)
         username = credential_dict["username"]
         password = credential_dict["password"]
