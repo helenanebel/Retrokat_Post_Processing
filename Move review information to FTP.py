298ffba9055c901cec1133580e6a497c3b3c0619
Helena Nebel <helena.nebel@uni-tuebingen.de>
2022-11-08 10:51:01 +0100

    Add username on BENU from file

--- Move review information to FTP.py
+++ Move review information to FTP.py
@@ -1,7 +1,9 @@
 import os
 from shutil import copy2
 from time import strftime
+from get_name import get_name
 
+BENU_username = get_name()
 move_to_scp_server = ''
 commands = ''
 timestamp = strftime('%Y%m%d')
@@ -14,9 +16,8 @@ for file in os.listdir('review_information'):
         copy2('review_information/' + file, 'W:/FID-Projekte/Team Retro-Scan/Zotero/Einspielen_Sonstiges/' + file)
         move_to_scp_server += '\npython3 upload_to_bsz_ftp_server.py ' + file + ' /pub/UBTuebingen_Sonstiges/'
 
-
-print('Commands:')
-print('Dateien auf BENU verschieben & von BENU auf den FTP-Server legen:'
-      '\nW:\ncd /FID-Projekte/Team Retro-Scan/Zotero/Einspielen_Sonstiges'
-      '\nscp ' + timestamp + '_*.csv hnebel@benu.ub.uni-tuebingen.de:/home/hnebel/bsz'
-      '\n\nssh hnebel@benu.ub.uni-tuebingen.de\ncd /home/hnebel/bsz' + move_to_scp_server)
+commands = 'Dateien auf BENU verschieben & von BENU auf den FTP-Server legen:' \
+           '\nW:\ncd /FID-Projekte/Team Retro-Scan/Zotero/Einspielen_Sonstiges' \
+           '\nscp ' + timestamp + '_*.csv {0}@benu.ub.uni-tuebingen.de:/home/{0}/bsz' \
+           '\n\nssh {0}@benu.ub.uni-tuebingen.de\ncd /home/{0}/bsz' + move_to_scp_server
+print(commands.format(BENU_username))
